fe31e7111376e5bf0977a903f39822b570e27cf6
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2024-12-31 23:18:14 +0100

    created admin class

---
+++ classes/Admin.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/User.php';
+
+class Admin extends User{
+    public function __construct($id, $fname, $lname, $email, $password, $createdAt, $updatedAt){
+        parent::__construct($id, $fname, $lname, $email, $password, 'admin', $createdAt, $updatedAt);
+    }
+}
