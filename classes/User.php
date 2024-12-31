887616ad35c4292d0ebe38117e39afa6c7e05270
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-01 00:25:53 +0100

    added validations to setters in user class

--- classes/User.php
+++ classes/User.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/Database.php';
+require_once __DIR__.'/../exceptions/InputException.php';
 
 class User{
     protected $id;
@@ -14,15 +15,19 @@ class User{
     protected $database;
 
     public function __construct($id, $fname, $lname, $email, $password, $role, $createdAt, $updatedAt){
-        $this->setId($id);
-        $this->setFname($fname);
-        $this->setLname($lname);
-        $this->setEmail($email);
-        $this->setPassword($password);
-        $this->setRole($role);
-        $this->setCreatedAt($createdAt);
-        $this->setUpdatedAt($updatedAt);
-        $this->database = new Database();
+        try{
+            $this->setId($id);
+            $this->setFname($fname);
+            $this->setLname($lname);
+            $this->setEmail($email);
+            $this->setPassword($password);
+            $this->setRole($role);
+            $this->createdAt = $createdAt;
+            $this->updatedAt = $updatedAt;
+            $this->database = new Database();
+        }catch(InputException $e){
+            echo "Error : " . $e->getMessage();
+        }
     }
 
     //getters
@@ -60,34 +65,44 @@ class User{
 
     //setters
     public function setId($id){
+        if($id != null)
+            if(!filter_var($id, FILTER_VALIDATE_INT))
+                throw new InputException('Id must be a number !');
         $this->id = $id;
     }
 
     public function setFname($fname){
+        if($fname != null)
+            if(!is_string($fname))
+                throw new InputException('First name must be a string !');
         $this->fname = $fname;
     }
 
     public function setLname($lname){
+        if($lname != null)
+            if(!is_string($lname))
+                throw new InputException('Last name must be a string !');
         $this->lname = $lname;
     }
 
     public function setEmail($email){
+        if($email != null)
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+                throw new InputException('Email isn\'t valid !');
         $this->email = $email;
     }
 
     public function setPassword($password){
+        if($password != null)
+            if(strlen($password) < 8)
+                throw new InputException('Password must contain at least 8 characters !');
         $this->password = $password;
     }
 
     public function setRole($role){
+        if($role != null)
+            if($role != 'admin' && $role != 'author' && $role != 'visitor')
+                throw new InputException('Role can only be admin, visitor or author !');
         $this->role = $role;
-    }
-
-    public function setCreatedAt($createdAt){
-        $this->createdAt = $createdAt;
-    }
-
-    public function setUpdatedAt($updatedAt){
-        $this->updatedAt = $updatedAt;
     }
 }
