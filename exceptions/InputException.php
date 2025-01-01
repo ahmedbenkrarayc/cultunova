9f8f30f6ad9260b18eb05f7dc30d51e96c90bf72
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-01 18:09:24 +0100

    customed message in EnvException class

--- exceptions/InputException.php
+++ exceptions/InputException.php
@@ -1,5 +1,7 @@
 <?php
 
 class InputException extends Exception{
-    
+    public function __construct($message, $code = 0, Exception $previous = null){
+        parent::__construct("Input value exception : ".$message, $code, $previous);
+    }   
 }
