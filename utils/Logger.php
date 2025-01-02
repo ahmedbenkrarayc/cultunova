b9b2a6324e873807e9194b664c5d9657b30f7e45
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-02 14:33:46 +0100

    corrected error_log method in Logger

--- utils/Logger.php
+++ utils/Logger.php
@@ -4,7 +4,7 @@ class Logger{
     private const FILE_LOGS = __DIR__.'/../logs/logs.txt';
 
     public static function error_log($message){
-        $content = date('Y-m-d H:i:s').'\t'.$message.'\n';
-        file_put_contents($this->FILE_LOGS, $content, FILE_APPEND);
+        $content = date('Y-m-d H:i:s')."\t".$message."\n";
+        file_put_contents(self::FILE_LOGS, $content, FILE_APPEND);
     }
 }
