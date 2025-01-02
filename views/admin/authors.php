fd2b8c317576a431d9d68c9196a0e80a1aca7db4
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-02 21:37:02 +0100

    created interface for displaying user profiles

---
+++ views/admin/authors.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Categories | Cultunova</title>
+    <!-- CSS files -->
+    <link href="./../../dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/demo.min.css?1692870487" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+  </head>
+  <body >
+    <script src="./../../dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page">
+    <!-- header here -->
+        <?php require_once './../../utils/__header.php' ?>
+        <div class="page-wrapper">
+        <!-- Page header -->
+        <div class="page-header d-print-none">
+          <div class="container-xl">
+            <div class="row g-2 align-items-center">
+              <div class="col">
+                <h2 class="page-title">
+                  Authors
+                </h2>
+              </div>
+              <!-- Page title actions -->
+              <div class="col-auto ms-auto d-print-none">
+                <div class="d-flex">
+                  <div class="me-3">
+                    <div class="input-icon">
+                      <input type="text" value="" class="form-control" placeholder="Search…">
+                      <span class="input-icon-addon">
+                        <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
+                      </span>
+                    </div>
+                  </div>
+                  <a href="#" class="btn btn-primary">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+                    Search
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- Page body -->
+        <div class="page-body">
+          <div class="container-xl">
+            <div class="row row-cards">
+              <div class="col-sm-6 col-lg-4">
+                <div class="card card-sm">
+                  <a href="#" class="d-block"><img src="./../../static/photos/beautiful-blonde-woman-relaxing-with-a-can-of-coke-on-a-tree-stump-by-the-beach.jpg" class="card-img-top"></a>
+                  <div class="card-body">
+                    <div class="d-flex align-items-center">
+                      <span class="avatar me-3 rounded" style="background-image: url(./../../static/avatars/000m.jpg)"></span>
+                      <div>
+                        <div>Paweł Kuna</div>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <?php require_once './../../utils/__footer.php' ?>
+      </div>
+    </div>
+    <!-- Libs JS -->
+    <script src="./../../dist/libs/list.js/dist/list.min.js?1692870487" defer></script>
+    <!-- Tabler Core -->
+    <script src="./../../dist/js/tabler.min.js?1692870487" defer></script>
+    <script src="./../../dist/js/demo.min.js?1692870487" defer></script>
+  </body>
+</html>
