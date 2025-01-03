597385c093f064489f1b835244f16edc012c2c2f
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-03 10:15:17 +0100

    created interface for article details

---
+++ views/author/article.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Categories | Cultunova</title>
+    <!-- CSS files -->
+    <link href="./../../dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/demo.min.css?1692870487" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+  </head>
+  <body >
+    <script src="./../../dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page">
+        <!-- header here -->
+        <?php require_once './../../utils/__header.php' ?>
+        <div class="page-wrapper">
+            <!-- Page body -->
+            <div class="page-body">
+                <div class="container-xl">
+                    <div class="row row-cards">
+                        <div class="col-md-12">
+                            <div class="card">
+                            <img style="height: 200px; object-fit: cover;" src="https://images.unsplash.com/photo-1730119986244-eb33b57b3950?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+                            <div class="card-body">
+                                <div class="row g-2 align-items-center">
+                                <div class="col-auto">
+                                    <span class="avatar avatar-lg" style="background-image: url(./static/avatars/000m.jpg)"></span>
+                                </div>
+                                <div class="col">
+                                    <h4 class="card-title m-0">
+                                    <a href="#">Paweł Kuna</a>
+                                    </h4>
+                                    <div class="text-secondary">
+                                    Author
+                                    </div>
+                                </div>
+                                <div class="col-auto">
+                                    <a href="#" class="btn btn-success">
+                                        Accept
+                                    </a>
+                                    <a href="#" class="btn btn-danger">
+                                        Reject
+                                    </a>
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row row-cards" style="margin-top: 20px;">
+                        <div class="col-lg-8">
+                            <div class="card card-lg">
+                            <div class="card-body">
+                                <div class="markdown">
+                                <p>This is a legal agreement between you, the Purchaser, and Tabler. Purchasing or downloading of any Tabler product (Tabler Free, Tabler PRO, Tabler Email), constitutes your acceptance of the terms of this license, <a href="https://tabler.io/terms-of-service.html">Tabler terms of service</a> and <a href="https://tabler.io/privacy-policy.html">Tabler private policy</a>.</p>
+                                <p>A license grants you a non-exclusive and non-transferable right to use and incorporate the item in your personal or commercial projects.</p>
+                                <h3 id="tabler-free-license">Tabler Free License</h3>
+                                <p>Tabler Free is available under MIT License</p>
+                                <p>Copyright 2023 Tabler</p>
+                                <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+                                <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+                                <p>THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
+                                <h3 id="tabler-pro-and-tabler-email-license">Tabler PRO and Tabler Email License</h3>
+                                <p>After Purchasing you are granted the use products under the conditions featured belowed.</p>
+                                <p>Rights</p>
+                                <ol>
+                                    <li>You have rights to use our resources for any or all of your personal and commercial projects.</li>
+                                    <li>You may modify the resources according to your requirements.</li>
+                                    <li>You are not required to attribute or link to Tabler in any of your projects.</li>
+                                </ol>
+                                <p>Restrictions</p>
+                                <ol>
+                                    <li>You do not have the rights to redistribute, resell, lease, license, sub-license or offer the file downloaded to any third party.</li>
+                                    <li>For any resalable web applications or software programs, you cannot include our graphic resources as an additional attachment.</li>
+                                    <li>You cannot redistribute any of the software, or products created with Tabler paid  products.</li>
+                                    <li>You cannot add our source code to any open source repository.</li>
+                                    <li>The source code may not be placed on any website in a complete or archived downloadable format.</li>
+                                </ol>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center mb-3">
+                                    <div class="me-3">
+                                        <!-- Download SVG icon from http://tabler-icons.io/i/scale -->
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-md" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 20l10 0" /><path d="M6 6l6 -1l6 1" /><path d="M12 3l0 17" /><path d="M9 12l-3 -6l-3 6a3 3 0 0 0 6 0" /><path d="M21 12l-3 -6l-3 6a3 3 0 0 0 6 0" /></svg>
+                                    </div>
+                                    <div>
+                                        <h3 class="lh-1">Title</h3>
+                                    </div>
+                                    </div>
+                                    <div class="text-secondary mb-3">
+                                    A short and simple permissive license with conditions only requiring preservation of copyright and
+                                    license notices. Licensed works, modifications, and larger works may be distributed under different terms
+                                    and without source code.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
+            <?php require_once './../../utils/__footer.php' ?>
+            </div>
+        </div>
+    </div>
+    <!-- Libs JS -->
+    <script src="./../../dist/libs/list.js/dist/list.min.js?1692870487" defer></script>
+    <!-- Tabler Core -->
+    <script src="./../../dist/js/tabler.min.js?1692870487" defer></script>
+    <script src="./../../dist/js/demo.min.js?1692870487" defer></script>
+  </body>
+</html>
