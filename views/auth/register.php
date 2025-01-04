fa1f5a21cdee58d7ef05a1c3db5d668a65d47e9b
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-04 20:01:47 +0100

    created login page design

---
+++ views/auth/register.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Sign in - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+    <!-- CSS files -->
+    <link href="./../../dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-payments.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/tabler-vendors.min.css?1692870487" rel="stylesheet"/>
+    <link href="./../../dist/css/demo.min.css?1692870487" rel="stylesheet"/>
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+      body {
+      	font-feature-settings: "cv03", "cv04", "cv11";
+      }
+    </style>
+  </head>
+  <body  class=" d-flex flex-column">
+    <script src="./../../dist/js/demo-theme.min.js?1692870487"></script>
+    <div class="page page-center">
+      <div class="container container-tight py-4">
+        <div class="card card-md">
+          <div class="card-body">
+            <h2 class="h2 text-center mb-4">Create new account</h2>
+            <form action="./../../" method="get" autocomplete="off" novalidate>
+              <div class="mb-3">
+                <label class="form-label">Email address</label>
+                <input type="email" class="form-control" placeholder="your@email.com" autocomplete="off">
+              </div>
+              <div class="mb-2">
+                <label class="form-label">
+                  Password
+                </label>
+                <div class="input-group input-group-flat">
+                  <input type="password" class="form-control"  placeholder="Your password"  autocomplete="off">
+                </div>
+              </div>
+              <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">Create new account</button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="text-center text-secondary mt-3">
+          Already have an account? <a href="./login.php" tabindex="-1">Sign in</a>
+        </div>
+      </div>
+    </div>
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="./../../dist/js/tabler.min.js?1692870487" defer></script>
+    <script src="./../../dist/js/demo.min.js?1692870487" defer></script>
+  </body>
+</html>
