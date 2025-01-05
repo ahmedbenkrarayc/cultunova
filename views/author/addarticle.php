275ef0fa920e74b4c8df1cef9d705c5c80a4c9b1
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-05 23:07:03 +0100

    implemented update article in editarticle view

--- views/author/addarticle.php
+++ views/author/addarticle.php
@@ -36,7 +36,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
-    <title>Categories | Cultunova</title>
+    <title>Create article | Cultunova</title>
     <!-- CSS files -->
     <link href="./../../dist/css/tabler.min.css?1692870487" rel="stylesheet"/>
     <link href="./../../dist/css/tabler-flags.min.css?1692870487" rel="stylesheet"/>
