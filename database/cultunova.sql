a37ef88f8a2176cd40dd2af6ecd19f42cb0551e4
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-06 22:50:28 +0100

    created v2 of database

--- database/cultunova.sql
+++ database/cultunova.sql
@@ -62,3 +62,83 @@ SELECT * FROM pub30;
 #Trouver les catégories qui n'ont aucun article associé
 SELECT * FROM category c LEFT JOIN article a ON c.id = a.category_id WHERE a.category_id IS NULL;
 */
+
+CREATE DATABASE cultunovav2;
+USE cultunovav2;
+
+CREATE TABLE user(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	fname VARCHAR(50),
+	lname VARCHAR(50),
+	email VARCHAR(100) UNIQUE,
+	password VARCHAR(250),
+	role ENUM('admin', 'author', 'visitor'),
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE author_details(
+	author_id INT PRIMARY KEY,
+	picture TEXT,
+	cover TEXT,
+	deleted TINYINT(1) DEFAULT 0,
+	FOREIGN KEY(author_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE category(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE tag(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW()
+);
+
+CREATE TABLE article(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(50) NOT NULL,
+	description TEXT,
+	content TEXT,
+	cover TEXT,
+	status ENUM('accepted', 'in review', 'rejected') DEFAULT 'in review',
+	category_id INT,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW(),
+	FOREIGN KEY(category_id) REFERENCES category(id) ON DELETE CASCADE
+);
+
+CREATE TABLE articletag(
+	article_id INT,
+	tag_id INT,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+	FOREIGN KEY(article_id) REFERENCES article(id) ON DELETE CASCADE,
+	FOREIGN KEY(tag_id) REFERENCES tag(id) ON DELETE CASCADE,
+	PRIMARY KEY(article_id, tag_id)
+);
+
+CREATE TABLE likes(
+	article_id INT,
+	visitor_id INT,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+	FOREIGN KEY(article_id) REFERENCES article(id) ON DELETE CASCADE,
+	FOREIGN KEY(visitor_id) REFERENCES user(id) ON DELETE CASCADE,
+	PRIMARY KEY(article_id, visitor_id)
+);
+
+CREATE TABLE comment(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	content TEXT,
+	article_id INT,
+	visitor_id INT,
+	createdAt DATETIME DEFAULT NOW(),
+	updatedAt DATETIME DEFAULT NOW() ON UPDATE NOW(),
+	FOREIGN KEY(article_id) REFERENCES article(id) ON DELETE CASCADE,
+	FOREIGN KEY(visitor_id) REFERENCES user(id) ON DELETE CASCADE
+);
