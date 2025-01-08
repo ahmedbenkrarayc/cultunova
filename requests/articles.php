0dbc8c8b45c2c56bfa368b2590fd17699baea404
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-01-08 23:34:29 +0100

    created action to handle reading articles for js

--- requests/articles.php
+++ requests/articles.php
@@ -1,2 +1,17 @@
 <?php
 
+require_once __DIR__.'/../classes/Article.php';
+
+$article = new Article(null, null, null, null, null, null, null, null, null, null);
+$articles = $article->getAll();
+
+if($articles !== null){
+    echo json_encode([
+        'success' => true,
+        'data' => $articles
+    ]);
+}else{
+    echo json_encode([
+        'success' => false
+    ]);
+}
